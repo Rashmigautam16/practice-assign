99a5648d45fd0a75e00240a44e3ff6aa609241db
Rashmigautam16 <rashmigaut99@gmail.com>
2023-01-08 20:03:00 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Full stack web developer</h1>
+        <h3>Next Js Work</h3>
       </div>
     </>
   )
